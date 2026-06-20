91f61391aca4bf6eef07df42fd470a1b56f3cdfb
kumarkumar95599-rgb <kumarkumar95599@gmail.com>
2026-06-20 23:10:47 +0530

    Add GET endpoint to ask route

--- routes/ask.js
+++ routes/ask.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Ask route working"
+  });
+});
+
 router.post("/", async (req, res) => {
   res.json({
     success: true,
